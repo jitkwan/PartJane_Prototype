a03246e23f7a30dcc5c811dc31218cfbba5d7fa5
jitkwan <jitkwan.y@gmail.com>
2022-12-03 17:16:25 +0700

    edit function of formvalidate no color

--- src/Components/FormComponent.jsx
+++ src/Components/FormComponent.jsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 //state declare of each infomation
-const FormComponent = (props) =>{
+const FormComponent1 = (props) =>{
     const [activityName,setActivityName] = useState('')
     const [activityType,setActivityType] = useState('')
     const [description,setDescription] = useState('')
@@ -20,10 +20,10 @@ const FormComponent = (props) =>{
     const [errorDate,setErrorDate] = useState('')
 
 //state declare to change color if user don't put the information
-    const [activityNameColor,setActivityNameColor] = useState('')
-    const [activityTypeColor,setActivityTypeColor] = useState('')
-    const [durationColor,setDurationColor] = useState('')
-    const [dateColor,setDateColor] = useState('')
+    // const [activityNameColor,setActivityNameColor] = useState('')
+    // const [activityTypeColor,setActivityTypeColor] = useState('')
+    // const [durationColor,setDurationColor] = useState('')
+    // const [dateColor,setDateColor] = useState('')
 
     //validate part
     const submitData = (e) =>{
@@ -31,35 +31,33 @@ const FormComponent = (props) =>{
 
         if(activityName.length > 0){
             setActivityName(activityName)
-            setActivityNameColor('green')
+            setErrorActivityName('')
         }else{
             setErrorActivityName('Please specific your activity name')
-            setActivityNameColor('red')
         }
 
-        if(activityType !== 'Activity Type'){
+        if(activityType !== '' && activityType !== 'Please Select'){
             setActivityType(activityType)
-            setActivityTypeColor('green')
+            setErrorActivityType('')
         }else{
             setErrorActivityType('Please select the activity type')
-            setActivityTypeColor('red')
-        }
-        if(duration !== '' && duration >= 10 && duration <= 360 ){
-            setDuration(duration)
-            setDurationColor('green')
-        }else{
-            setErrorDuration('Please specific time')
-            setDurationColor('red')
-        }
-        if(date !== ''){
-            setDate(date)
-            setDateColor('green')
-        }else{
-            setErrorDate('Date is required')
-            setDateColor('red')
         }
 
-        if(activityNameColor==='green' && activityTypeColor==='green'&& durationColor==='green' && dateColor==='green'){
+        if(duration >= 10 ){
+            setDuration(duration)
+            setErrorDuration('')
+        }else{
+            setErrorDuration('Please specific time and you should excercise at least 10 minutes')
+        }
+
+        if(date !== ''){
+            setDate(date)
+            setErrorDate('')
+        }else{
+            setErrorDate('Date is required')
+        }
+
+        if(activityName.length>0 && activityType !=='Please Select'&& duration >= 10 && date !== ''){
             const newCard = {
                 id: uuidv4(),
                 activityName: activityName,
@@ -72,7 +70,7 @@ const FormComponent = (props) =>{
             props.onAddNewCard(newCard)
             
             setActivityName('')
-            setActivityType('Activity Type')
+            setActivityType('Please Select')
             setDescription('')
             setDuration('')
             setDate('')
@@ -80,18 +78,9 @@ const FormComponent = (props) =>{
             setErrorActivityType('')
             setErrorDuration('')
             setErrorDate('')
-            setActivityNameColor('')
-            setActivityTypeColor('')
-            setDurationColor('')
-            setDateColor('')
-
-
-       }
+        }
     }
 
-    // const submitData=(e)=>{
-    //     e.preventDefault()
-    //     const newItem = {  } }
 
     return(
         <div className="container">
@@ -100,38 +89,41 @@ const FormComponent = (props) =>{
 {/* for user to put Name of Activity */}
                 <div className= 'form-control'>
                     <label>Name Your Activity</label>
-                    <input type = 'text' placeholder = 'Specific your activity name' value={activityName} onChange = {(e) => setActivityName(e.target.value)} style={{borderColor:activityNameColor}}/>
-                    <small>{errorActivityName}</small>
+                    <input 
+                    type = 'text' 
+                    placeholder = 'Specific your activity name' 
+                    value={activityName} onChange = {(e) => setActivityName(e.target.value)}/>
+                    <small className='text-red-700'>{errorActivityName}</small>
                 </div>
 {/* for user to select type of Activity */}
                 <div>
-                    <select className="form-control" value={activityType} onChange = {(e) =>setActivityType(e.target.value)} style={{borderColor:activityTypeColor}}>
-                        <option value = 'Activity Type'>Activity Type</option>
+                    <label>Activity Type</label>
+                    <select className="form-control" value={activityType} onChange = {(e) =>setActivityType(e.target.value)} >
+                        <option value = 'Please Select'>Please Select</option>
                         <option value = 'Run'>Run</option>
                         <option value = 'Bicycle Ride'>Bicycle Ride</option>
                         <option value = 'Swim'>Swim</option>
                         <option value = 'Walk'>Walk</option>
                         <option value = 'Hike'>Hike</option>
                     </select>
-                    <small>{errorActivityType}</small>
+                    <small className='text-red-700'>{errorActivityType}</small>
                 </div>
 {/* for user to describe the Activity */}
                 <div className= 'form-control'>
                     <label>Description</label>
-                    <input type = 'text' placeholder = 'Describe your activity...' value={description} onChange = {(e) => setDescription(e.target.value)}/>
-                    {/* <small>{errorPassword}</small> */}
+                    <textarea type = 'text' placeholder = 'Describe your activity...' value={description} onChange = {(e) => setDescription(e.target.value)} cols="30" rows="5"></textarea>
                 </div>
 {/* for user to set duration */}
                 <div className= 'form-control'>
                     <label>Duration(between 10 - 360 minutes)</label>
-                    <input type ='number' placeholder = "Specific activity's time " value={duration} onChange = {(e) => setDuration(e.target.value)} style={{borderColor:durationColor}}/>
-                    <small>{errorDuration}</small>
+                    <input type ='number' placeholder = "Specific activity's time " value={duration} onChange = {(e) => setDuration(e.target.value)}/>
+                    <small className='text-red-700'>{errorDuration}</small>
                 </div>
 {/* for user to set date */}
                 <div className= 'form-control'>
                     <label>Date</label>
-                    <input type ='date' value={date} onChange = {(e)=> setDate(e.target.value)} style={{borderColor:dateColor}}/>
-                    <small>{errorDate}</small>
+                    <input type ='date' value={date} onChange = {(e)=> setDate(e.target.value)}/>
+                    <small className='text-red-700'>{errorDate}</small>
                 </div>
                 <button type='submit'>submit</button>
                 
@@ -144,4 +136,4 @@ const FormComponent = (props) =>{
 }
 
 
-export default FormComponent
+export default FormComponent1
